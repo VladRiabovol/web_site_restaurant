aed227a01ecc0e4a576b24b01a2c737c7ff70f90
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2021-11-01 15:10:04 +0200

    add slider for about_us.html and bot for send message with order to group

---
+++ bot/bot.py
@@ -0,0 +1,6 @@
+import telebot
+
+API_TOKEN = '2044074587:AAHSfy7aAvd8YsihpYGCyhw7lGdjQgYmeIs'
+CHAT_ID = '-654006828'
+
+bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
---
+++ order_restaurant/static/js/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.about_us_slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: true,
+        adaptiveHeight: false,
+        centerMode: true,
+        variableWidth: true,
+    });
+})
+
